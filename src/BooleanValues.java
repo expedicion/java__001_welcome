926d88856863888eb0c1a87dfa8650780a42746a
Nostromo <mks.nostromo@gmail.com>
2019-03-01 22:06:19 +0100

    Next subjects for simple Java codes.

---
+++ src/BooleanValues.java
@@ -0,0 +1,10 @@
+public class BooleanValues {
+    public static void main(String[] args) {
+        boolean booleanValue;
+
+        booleanValue = true;
+        System.out.println(booleanValue);
+        booleanValue = false;
+        System.out.println(booleanValue);
+    }
+}
